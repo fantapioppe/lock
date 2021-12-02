db8c02acf170cd1226be57ceab849cdb16500d1d
rikymori <riccardomori@MacBook-Air-STOC.local>
2021-12-02 19:23:39 +0100

    installazione angular-fire

--- lock/src/environments/environment.ts
+++ lock/src/environments/environment.ts
@@ -3,6 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'lock-7d8f1',
+    appId: '1:290165431405:web:74c528977afc0786f47364',
+    databaseURL: 'https://lock-7d8f1-default-rtdb.europe-west1.firebasedatabase.app',
+    storageBucket: 'lock-7d8f1.appspot.com',
+    apiKey: 'AIzaSyAd0cx75SypZXIo6de9b1r7QbbcvpfJzjo',
+    authDomain: 'lock-7d8f1.firebaseapp.com',
+    messagingSenderId: '290165431405',
+    measurementId: 'G-76L84T15H7',
+  },
   production: false
 };
 
